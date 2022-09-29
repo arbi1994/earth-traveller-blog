310b95d71f9aaf97dffd5ef233bb735ef01d1789
arbi1994 <arbi.1994@hotmail.com>
2022-09-29 18:32:26 +1000

    Modified Layout import from 'layout' to 'Layout' as it was giving me issues on production build

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import * as React from "react"
 import "../sass/index.scss"
 import * as styles from "./styles.module.scss"
 // components
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import SectionLayout from "../components/SectionLayout"
 import LatestArticles from "../components/LatestArticles"
